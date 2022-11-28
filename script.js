2453ef5d09f22d529c16517be07a9c716d9159e3
joefrankpaul <joefrank4christ@gmail.com>
2022-11-28 05:37:40 -0800

    first commit

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-console.log("start program")
-alert("hello");
-document.write("<h1>hello from this side");
-console.log("program complete");
-console.log("end the program");
-function randomNumber(upper) {
-    return Math.floor(Math.random() * upper) + 1;
-}
-var counter = 0;
-while (counter <10) {
-    var randNum=randomNumber (6);
-    document.write(randNum + ' ');
-    counter += 1;
-}
-var randomNumber = getRandomNumber(10);
-var guess;
-var guessCount = 0;
-varcorrectGuess = false
+// console.log("start program")
+// alert("hello");
+// document.write("<h1>hello from this side");
+// console.log("program complete");
+// console.log("end the program");
+// function randomNumber(upper) {
+//     return Math.floor(Math.random() * upper) + 1;
+// }
+// var counter = 0;
+// while (counter <10) {
+//     var randNum=randomNumber (6);
+//     document.write(randNum + ' ');
+//     counter += 1;
+// }
+// var randomNumber = getRandomNumber(10);
+// var guess;
+// var guessCount = 0;
+// varcorrectGuess = false
 
-function getRandomNumber(upper){
-    var num = Math.floor(Math.random() * upper) + 1;
-    return num;
-}
-do { 
-    guess = prompt('I am thinking of a number between 1 and 10, what is it')
-    guessCount += 1;
-    if (parseInt(guess)=== randomNumber){
-     correctGuess = true;
-    } 
-} while ( ! correctGuess )
+// function getRandomNumber(upper){
+//     var num = Math.floor(Math.random() * upper) + 1;
+//     return num;
+// }
+// do { 
+//     guess = prompt('I am thinking of a number between 1 and 10, what is it')
+//     guessCount += 1;
+//     if (parseInt(guess)=== randomNumber){
+//      correctGuess = true;
+//     } 
+// } while ( ! correctGuess )
 
-var html ='';
-for(var counter = 0; counter <1000; counter += 1){
-    html += '<div>' + i + '</div>';
-}
-document.write(html);
+// var html ='';
+// for(var counter = 0; counter <1000; counter += 1){
+//     html += '<div>' + i + '</div>';
+// }
+// document.write(html);
